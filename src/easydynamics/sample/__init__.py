720f1101b0c4084b9006b2fe437d50868ecbd387
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2025-06-12 15:16:08 +0200

    Add support for delta function and an example showing its use

--- src/easydynamics/sample/__init__.py
+++ src/easydynamics/sample/__init__.py
@@ -2,6 +2,7 @@ from .SampleModel import SampleModel
 from .components import GaussianComponent
 from .components import LorentzianComponent
 from .components import VoigtComponent
+from .components import DeltaFunctionComponent
 from .components import DHOComponent
 from .components import PolynomialComponent
 
@@ -10,5 +11,6 @@ __all__ = [
     "GaussianComponent",
     "LorentzianComponent",
     "VoigtComponent",
+    "DeltaFunctionComponent",
     "DHOComponent",
     "PolynomialComponent"]
