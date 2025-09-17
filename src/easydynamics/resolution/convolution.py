Rename LorentzianComponent and fix units

src/easydynamics/resolution/convolution.py
@@ -7,7 +7,7 @@ from scipy.special import voigt_profile
 from easydynamics.sample import DeltaFunctionComponent
 from easydynamics.sample.components import ModelComponent  
 from easydynamics.sample import Gaussian 
-from easydynamics.sample import LorentzianComponent
+from easydynamics.sample import Lorentzian
 from easydynamics.sample import SampleModel
 
 from easyscience.variable import Parameter
@@ -270,15 +270,15 @@ class ResolutionHandler:
             return True, self.gaussian_eval(x, center, width, area)
 
         # Lorentzian + Lorentzian --> Lorentzian
-        if isinstance(s, LorentzianComponent) and isinstance(r, LorentzianComponent):
+        if isinstance(s, Lorentzian) and isinstance(r, Lorentzian):
             width = s.width.value + r.width.value
             area  = s.area.value * r.area.value
             center = (s.center.value + r.center.value) + off
             return True, self.lorentzian_eval(x, center, width, area)
 
         # Gaussian + Lorentzian --> Voigt 
-        if (isinstance(s, Gaussian) and isinstance(r, LorentzianComponent)) or \
-           (isinstance(s, LorentzianComponent) and isinstance(r, Gaussian)):
+        if (isinstance(s, Gaussian) and isinstance(r, Lorentzian)) or \
+           (isinstance(s, Lorentzian) and isinstance(r, Gaussian)):
             if isinstance(s, Gaussian):
                 G, L = s, r
             else:

src/easydynamics/sample/components.py
@@ -230,6 +230,9 @@ class Gaussian(ModelComponent):
         return [self.area, self.center, self.width]
     
     def copy(self) -> "Gaussian":
+        """
+        Return a deep copy of this component with independent parameters.
+        """
 
         ModelCopy=Gaussian(
             name=self.name,
@@ -248,7 +251,7 @@ class Gaussian(ModelComponent):
         return f"GaussianComponent(name={self.name}, area={self.area}, center={self.center}, width={self.width})"
 
 
-class LorentzianComponent(ModelComponent):
+class Lorentzian(ModelComponent):
     """
     Lorentzian function. Creates new EasyScience Parameters if floats are provided, otherwise uses the provided Parameters.
 
@@ -258,7 +261,12 @@ class LorentzianComponent(ModelComponent):
         width (float): HWHM (Half Width at Half Maximum).
     """
 
-    def __init__(self, name='Lorentzian', area=1.0, center=None, width=1.0, unit='meV'):
+    def __init__(self, 
+                 name: str = 'Lorentzian', 
+                 area: Union[float, Parameter] = 1.0, 
+                 center: Union[float, Parameter, None] = None, 
+                 width: Union[float, Parameter] = 1.0, 
+                 unit: str = 'meV'):
 
         
         # Validate inputs
@@ -280,7 +288,6 @@ class LorentzianComponent(ModelComponent):
             if area < 0:
                 warnings.warn("The area of the Lorentzian with name {} is negative, which may not be physically meaningful.".format(name))
             area = float(area)
-
 
         if isinstance(center, Number):
             center = float(center)
@@ -306,13 +313,21 @@ class LorentzianComponent(ModelComponent):
         else:
             self.area=area
 
-    def evaluate(self, x):
-            #TODO: Handle units properly
+    def evaluate(self, x:Union[float,np.ndarray,sc.array]) -> Union[float,np.ndarray]:
         if self.width.value <= 0:
             raise ValueError("Width must be greater than 0 for Lorentzian.")
         if self.area.value < 0:
             warnings.warn("The area of the Lorentzian with name {} is negative, which may not be physically meaningful.".format(self.name))
-        return self.area.value * (self.width.value/np.pi / ((x - self.center.value)**2 + self.width.value**2))
+
+        # Handle units
+        if isinstance(x, sc.array):
+            x_in = x.values
+            if self.unit is not None and x.unit != self.unit:
+                warnings.warn(f"Input x has unit {x.unit}, but Lorentzian component has unit {self.unit}. Converting Lorentzian to {x.unit}.")
+                self.convert_unit(x.unit)
+        else:
+            x_in = x    
+        return self.area.value * (self.width.value/np.pi / ((x_in - self.center.value)**2 + self.width.value**2))
 
 
     def get_parameters(self):
@@ -323,9 +338,9 @@ class LorentzianComponent(ModelComponent):
         """ 
         return [self.area, self.center, self.width]
     
-    def copy(self) -> "LorentzianComponent":
+    def copy(self) -> "Lorentzian":
 
-        ModelCopy =LorentzianComponent(
+        ModelCopy =Lorentzian(
             name=self.name, 
             area=self.area.value,
             center=self.center.value,

tests/unit_tests/resolution/test_convolution.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.special import voigt_profile
 
 from easyscience.variable import Parameter
-from easydynamics.sample import SampleModel, Gaussian, LorentzianComponent, DeltaFunctionComponent
+from easydynamics.sample import SampleModel, Gaussian, Lorentzian, DeltaFunctionComponent
 
 
 from easydynamics.resolution import ResolutionHandler
@@ -18,7 +18,7 @@ class TestConvolution:
     @pytest.fixture
     def sample_model(self):
         test_sample_model = SampleModel(name="TestSampleModel")
-        test_sample_model.add_component(LorentzianComponent(center=0.1, width=0.2, area=2.0))
+        test_sample_model.add_component(Lorentzian(center=0.1, width=0.2, area=2.0))
         return test_sample_model
 
     @pytest.fixture
@@ -82,8 +82,8 @@ class TestConvolution:
     )
     def test_components_lorentzian_lorentzian(self, x, offset_obj, expected_shift, method):
         #WHEN
-        sample_lorentzian = LorentzianComponent(center=0.1, width=0.3, area=2)
-        resolution_lorentzian = LorentzianComponent(center=0.2, width=0.4, area=3)
+        sample_lorentzian = Lorentzian(center=0.1, width=0.3, area=2)
+        resolution_lorentzian = Lorentzian(center=0.2, width=0.4, area=3)
 
         resolution_handler = ResolutionHandler()
 
@@ -115,7 +115,7 @@ class TestConvolution:
     def test_components_gauss_lorentzian(self, x, offset_obj, expected_shift, method):
         #WHEN
         sample_gauss = Gaussian(center=0.1, width=0.3, area=2)
-        resolution_lorentzian = LorentzianComponent(center=0.2, width=0.4, area=3)
+        resolution_lorentzian = Lorentzian(center=0.2, width=0.4, area=3)
 
         resolution_handler = ResolutionHandler()
 
@@ -150,7 +150,7 @@ class TestConvolution:
     def test_components_lorentzian_gauss(self, x, offset_obj, expected_shift, method):
         #WHEN
         resolution_gauss = Gaussian(center=0.1, width=0.3, area=2)
-        sample_lorentzian = LorentzianComponent(center=0.2, width=0.4, area=3)
+        sample_lorentzian = Lorentzian(center=0.2, width=0.4, area=3)
 
         resolution_handler = ResolutionHandler()
 
@@ -284,7 +284,7 @@ class TestConvolution:
     )
     def test_model_lorentzian_delta_resolution_gauss(self, x, method):
         #WHEN
-        sample_lorentzian = LorentzianComponent(center=0.1, width=0.3, area=2, name="SampleLorentzian")
+        sample_lorentzian = Lorentzian(center=0.1, width=0.3, area=2, name="SampleLorentzian")
         sample_delta = DeltaFunctionComponent(center=0.5, area=4, name="SampleDelta")
         resolution_gauss = Gaussian(center=-0.3, width=0.4, area=3, name="ResolutionGauss")
         sample = SampleModel(name="SampleModel")

tests/unit_tests/sample/test_components.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from scipy.integrate import simpson
 
-from easydynamics.sample import Gaussian, LorentzianComponent, VoigtComponent, DeltaFunctionComponent, DHOComponent, PolynomialComponent
+from easydynamics.sample import Gaussian, Lorentzian, VoigtComponent, DeltaFunctionComponent, DHOComponent, PolynomialComponent
 from easydynamics.sample.components import ModelComponent
 
 from easyscience.variable import Parameter
@@ -95,22 +95,22 @@ class TestLorentzianComponent:
 
     @pytest.fixture
     def lorentzian(self):
-        return LorentzianComponent(name='TestLorentzian', area=2.0, center=0.5, width=0.6, unit='meV')
+        return Lorentzian(name='TestLorentzian', area=2.0, center=0.5, width=0.6, unit='meV')
 
-    def test_initialization(self, lorentzian: LorentzianComponent):
+    def test_initialization(self, lorentzian: Lorentzian):
         assert lorentzian.name == 'TestLorentzian'
         assert lorentzian.area.value == 2.0
         assert lorentzian.center.value == 0.5
         assert lorentzian.width.value == 0.6
         assert lorentzian.unit == 'meV'
 
-    def test_evaluate(self, lorentzian: LorentzianComponent):
+    def test_evaluate(self, lorentzian: Lorentzian):
         x = np.array([0.0, 0.5, 1.0])
         expected = lorentzian.evaluate(x)
         expected_result = (2.0 / (np.pi * 0.6)) / (1 + ((x - 0.5) / 0.6) ** 2)
         np.testing.assert_allclose(expected, expected_result, rtol=1e-5)
 
-    def test_get_parameters(self, lorentzian: LorentzianComponent):
+    def test_get_parameters(self, lorentzian: Lorentzian):
         params = lorentzian.get_parameters()
         assert len(params) == 3
         assert params[0].name == 'TestLorentzian area'
@@ -118,7 +118,7 @@ class TestLorentzianComponent:
         assert params[2].name == 'TestLorentzian width'
         assert all(isinstance(param, Parameter) for param in params)
 
-    def test_area_matches_parameter(self, lorentzian: LorentzianComponent):
+    def test_area_matches_parameter(self, lorentzian: Lorentzian):
         # WHEN
         x = np.linspace(lorentzian.center.value - 500 * lorentzian.width.value, lorentzian.center.value + 500 * lorentzian.width.value, 20000) #Lorentzians have very long tails
         y = lorentzian.evaluate(x)

tests/unit_tests/sample/test_sample_model.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.integrate import simpson
 
 from easyscience.variable import Parameter
-from easydynamics.sample import SampleModel, Gaussian, LorentzianComponent
+from easydynamics.sample import SampleModel, Gaussian, Lorentzian
 from easydynamics.sample.components import ModelComponent
 from easydynamics.utils import detailed_balance_factor
 
@@ -130,7 +130,7 @@ class TestSampleModel:
     def test_evaluate(self, sample_model):
         # When
         component1 = Gaussian(name="Gaussian1", area=1.0, center=0.0, width=1.0, unit='meV')
-        component2 = LorentzianComponent(name="Lorentzian1", area=2.0, center=1.0, width=0.5, unit='meV')
+        component2 = Lorentzian(name="Lorentzian1", area=2.0, center=1.0, width=0.5, unit='meV')
         sample_model.add_component(component1)
         sample_model.add_component(component2)
         # Then
@@ -144,7 +144,7 @@ class TestSampleModel:
         # When
         sample_model.temperature = 300
         component1 = Gaussian(name="Gaussian1", area=1.0, center=0.0, width=1.0, unit='meV')
-        component2 = LorentzianComponent(name="Lorentzian1", area=2.0, center=1.0, width=0.5, unit='meV')
+        component2 = Lorentzian(name="Lorentzian1", area=2.0, center=1.0, width=0.5, unit='meV')
         sample_model.add_component(component1)
         sample_model.add_component(component2)
         x = np.linspace(-5, 5, 100)
@@ -158,7 +158,7 @@ class TestSampleModel:
     def test_evaluate_component(self, sample_model):
         # When
         component1 = Gaussian(name="TestGaussian", area=1.0, center=0.0, width=1.0, unit='meV')
-        component2 = LorentzianComponent(name="TestLorentzian", area=2.0, center=1.0, width=0.5, unit='meV')
+        component2 = Lorentzian(name="TestLorentzian", area=2.0, center=1.0, width=0.5, unit='meV')
         sample_model.add_component(component1)
         sample_model.add_component(component2)
 
@@ -175,7 +175,7 @@ class TestSampleModel:
     def test_evaluate_component_with_detailed_balance(self, sample_model):
         # When
         component1 = Gaussian(name="TestGaussian", area=1.0, center=0.0, width=1.0, unit='meV')
-        component2 = LorentzianComponent(name="TestLorentzian", area=2.0, center=1.0, width=0.5, unit='meV')
+        component2 = Lorentzian(name="TestLorentzian", area=2.0, center=1.0, width=0.5, unit='meV')
         sample_model.add_component(component1)
         sample_model.add_component(component2)
         sample_model.temperature = 300