95ada73a61d2b922995dc7adb0e4bbce9fa6e211
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2025-09-18 13:52:29 +0200

    add some dummy code to check if code coverage works

--- tests/unit_tests/test_dummy.py
+++ tests/unit_tests/test_dummy.py
@@ -2,6 +2,12 @@ import pytest
 
 class DummyTest:
 
-	def test_1_equals_1
+	def test_1_equals_1(self):
 		assert 1==1
+
+	def test_add_numbers(self):
+		from dummy_code import add_numbers
+		assert add_numbers(2, 3) == 5
+		assert add_numbers(-1, 1) == 0
+		assert add_numbers(0, 0) == 0
 		
