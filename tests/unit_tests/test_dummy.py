746ec29542380f05d18a95ae34fd66dc22f3548d
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2025-09-18 14:32:09 +0200

    change import

--- tests/unit_tests/test_dummy.py
+++ tests/unit_tests/test_dummy.py
@@ -6,7 +6,7 @@ def test_1_equals_1():
 	assert 1==1
 
 def test_add_numbers():
-	from src.easydynamics.dummy_code import add_numbers
+	from easydynamics.dummy_code import add_numbers
 	assert add_numbers(2, 3) == 5
 	assert add_numbers(-1, 1) == 0
 	assert add_numbers(0, 0) == 0
