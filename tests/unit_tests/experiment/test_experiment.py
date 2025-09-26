f8b43b7ff7fbcf17836d46ee87303fbe8542a732
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2025-09-26 15:44:52 +0200

    Test dunder and other help methods

--- tests/unit_tests/experiment/test_experiment.py
+++ tests/unit_tests/experiment/test_experiment.py
@@ -109,3 +109,60 @@ class TestExperiment:
         assert sc.identical(
             data.data, sc.array(dims=["Q", "energy"], values=np.ones((5, 6)) * 2)
         )
+
+    # Test helpful and dunder methods
+
+    def test_items(self, experiment):
+        items = list(experiment.items())
+        assert len(items) == 1
+        name, data = items[0]
+        assert name == "test_data"
+        assert isinstance(data, sc.DataArray)
+
+    def test_values(self, experiment):
+        values = list(experiment.values())
+        assert len(values) == 1
+        data = values[0]
+        assert isinstance(data, sc.DataArray)
+
+    def test_keys(self, experiment):
+        keys = list(experiment.keys())
+        assert keys == ["test_data"]
+
+    def test_dunder_methods(self, experiment):
+        # __getitem__
+        data = experiment["test_data"]
+        assert isinstance(data, sc.DataArray)
+
+        # __setitem__
+        Q = sc.linspace("Q", 2.0, 3.0, num=4, unit="1/Angstrom")
+        energy = sc.linspace("energy", -15, 15, num=5, unit="meV")
+        values = sc.array(dims=["Q", "energy"], values=np.ones((4, 5)) * 3)
+        set_data = sc.DataArray(data=values, coords={"Q": Q, "energy": energy})
+        experiment["set_data"] = set_data
+        assert "set_data" in experiment._data
+
+        # __delitem__
+        del experiment["set_data"]
+        assert "set_data" not in experiment._data
+
+        # __contains__
+        assert "test_data" in experiment
+        assert "non_existent" not in experiment
+
+        # __repr__ and __str__
+        repr_str = repr(experiment)
+        str_str = str(experiment)
+        assert "Experiment(name = test_experiment" in repr_str
+        assert repr_str == str_str
+
+        # Final check of data integrity
+        data = experiment.get_data("test_data")
+        assert isinstance(data, sc.DataArray)
+        assert "Q" in data.dims
+        assert "energy" in data.dims
+        assert data.sizes["Q"] == 10
+        assert data.sizes["energy"] == 11
+        assert sc.identical(
+            data.data, sc.array(dims=["Q", "energy"], values=np.ones((10, 11)))
+        )
