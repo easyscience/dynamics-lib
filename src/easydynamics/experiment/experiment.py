0332e946ab30225c59ea8bc6ab95c5e74bdc2d30
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2025-09-26 15:07:45 +0200

    Improve example and add more methods

--- src/easydynamics/experiment/experiment.py
+++ src/easydynamics/experiment/experiment.py
@@ -1,6 +1,9 @@
 from easyscience.job.experiment import ExperimentBase
 
 import scipp as sc
+import plopp as pp
+
+from IPython.display import display
 
 
 class Experiment(ExperimentBase):
@@ -10,72 +13,65 @@ class Experiment(ExperimentBase):
         """
         super().__init__(name)
         self._data = None
+        self._data = {}  # store data as {name: DataArray}
 
-    def load_csv(self, filename: str):
-        """
-        Load data from a CSV file.
-
-        Args:
-            file_path (str): Path to the data file.
-        """
-        self.append_data(sc.io.load_csv(filename))
-
-        # TODO: Add checks of dimensions etc.
-
-    def load_hdf5(self, filename: str):
+    def load_hdf5(self, filename: str, name: str):
         """
         Load data from an HDF5 file.
 
         Args:
             file_path (str): Path to the data file.
         """
-        self.append_data(sc.io.load_hdf5(filename))
+        self.append_data(sc.io.load_hdf5(filename), name)
 
         # TODO: Add checks of dimensions etc.
 
-    def append_data(self, new_data: sc.DataArray):
-        """
-        Append new data to the existing data.
+    def append_data(self, new_data: sc.DataArray, name: str):
+        """Append data with a name."""
+        self._data[name] = new_data
 
-        Args:
-            new_data (sc.DataArray): New data to append.
-        """
-        if self._data is None:
-            self._data = [new_data]
-        else:
-            self._data.append(new_data)
-
-    def get_data(self):
-        """
-        Get the stored data.
-
-        Returns:
-            : The experimental data.
-        """
-        return self._data
+    def get_data(self, name: str = None):
+        """Return the stored data. If name is None, return the full dict."""
+        if name is None:
+            return self._data
+        return self._data[name]
 
     def remove_all_data(self):
-        """
-        Remove the stored data.
-        """
-        self._data = None
+        self._data = {}
 
-    def remove_outliers(self):
-        """
-        Remove outliers from the data.
-
-        Placeholder.
-
-        """
-        raise NotImplementedError("Outlier removal is not implemented yet.")
-
-    def plot_data(self):
-        """ "
-        Plot the data
-        """
-
-        if self._data is None:
+    def plot_data(self, name: str = None):
+        """Plot all datasets."""
+        if not self._data:
             raise ValueError("No data to plot. Please load data first.")
 
-        for i, data in enumerate(self._data):
-            sc.plot(data, title=f"Data {i + 1}")
+        if name:
+            data = self._data.get(name)
+            if data is None:
+                raise ValueError(f"No data found for name: {name}")
+            fig = pp.plot(data.transpose(), title=f"{name}")
+            display(fig)
+            return
+
+        for name, data in self._data.items():
+            fig = pp.plot(data.transpose(), title=f"{name}")
+            display(fig)
+
+    # Dunder methods
+    def __getitem__(self, key: str):
+        """Allow dictionary-style access: my_exp['vanadium']"""
+        return self._data[key]
+
+    def __setitem__(self, key: str, value: sc.DataArray):
+        """Allow dictionary-style setting: my_exp['vanadium'] = data"""
+        self._data[key] = value
+
+    def __delitem__(self, key: str):
+        """Allow dictionary-style deletion: del my_exp['vanadium']"""
+        del self._data[key]
+
+    def __contains__(self, key: str):
+        """Allow use of 'in' keyword: 'vanadium' in my_exp"""
+        return key in self._data
+
+    def __repr__(self):
+        return f"Experiment(name = {self.name}, datasets={list(self._data.keys())})"
