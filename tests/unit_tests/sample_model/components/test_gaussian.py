f89a218a9259de706eb835e4e44f3aedb6c33aaa
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2025-09-30 15:14:39 +0200

    Update tests of gaussian

--- tests/unit_tests/sample_model/components/test_gaussian.py
+++ tests/unit_tests/sample_model/components/test_gaussian.py
@@ -51,32 +51,86 @@ class TestGaussian:
         with pytest.warns(UserWarning, match="may not be physically meaningful"):
             Gaussian(name="TestGaussian", area=-2.0, center=0.5, width=0.6, unit="meV")
 
+    def test_area_property_getter(self, gaussian: Gaussian):
+        assert gaussian.area.value == 2.0
+
+    def test_area_property_setter(self, gaussian: Gaussian):
+        # WHEN
+        gaussian.area = 3.0
+
+        # THEN EXPECT
+        assert gaussian.area.value == 3.0
+        with pytest.raises(TypeError, match="area must be a number."):
+            gaussian.area = "invalid"
+
+    def test_center_property_getter(self, gaussian: Gaussian):
+        assert gaussian.center.value == 0.5
+
+    def test_center_property_setter(self, gaussian: Gaussian):
+        # WHEN
+        gaussian.center = 0.6
+
+        # THEN EXPECT
+        assert gaussian.center.value == 0.6
+        with pytest.raises(TypeError, match="center must be a number."):
+            gaussian.center = "invalid"
+
+    def test_width_property_getter(self, gaussian: Gaussian):
+        assert gaussian.width.value == 0.6
+
+    def test_width_property_setter(self, gaussian: Gaussian):
+        # WHEN
+        gaussian.width = 0.7
+
+        # THEN EXPECT
+        assert gaussian.width.value == 0.7
+        with pytest.raises(TypeError, match="width must be a number."):
+            gaussian.width = "invalid"
+
     def test_evaluate(self, gaussian: Gaussian):
+        # WHEN
         x = np.array([0.0, 0.5, 1.0])
-        expected = gaussian.evaluate(x)
+
+        # THEN
+        result = gaussian.evaluate(x)
+
+        # EXPECT
         expected_result = (2.0 / (0.6 * np.sqrt(2 * np.pi))) * np.exp(
             -0.5 * ((x - 0.5) / 0.6) ** 2
         )
-        np.testing.assert_allclose(expected, expected_result, rtol=1e-5)
+        np.testing.assert_allclose(result, expected_result, rtol=1e-5)
 
     def test_evaluate_scipp_array(self, gaussian: Gaussian):
+        # WHEN
         x = sc.array(dims=["x"], values=[0.0, 0.5, 1.0], unit="meV")
-        expected = gaussian.evaluate(x)
+
+        # THEN
+        result = gaussian.evaluate(x)
+
+        # EXPECT
         expected_result = (2.0 / (0.6 * np.sqrt(2 * np.pi))) * np.exp(
             -0.5 * ((x.values - 0.5) / 0.6) ** 2
         )
-        np.testing.assert_allclose(expected, expected_result, rtol=1e-5)
+        np.testing.assert_allclose(result, expected_result, rtol=1e-5)
 
     def test_evaluate_with_different_unit(self, gaussian: Gaussian):
+        # WHEN
         x = sc.array(dims=["x"], values=[0.0, 500.0, 1000.0], unit="microeV")
-        expected = gaussian.evaluate(x)
+
+        # THEN
+        result = gaussian.evaluate(x)
+
+        # EXPECT
         expected_result = (2.0 * 1e3 / (0.6 * 1e3 * np.sqrt(2 * np.pi))) * np.exp(
             -0.5 * ((x.values - 500.0) / (0.6 * 1e3)) ** 2
         )
-        np.testing.assert_allclose(expected, expected_result, rtol=1e-5)
+        np.testing.assert_allclose(result, expected_result, rtol=1e-5)
 
     def test_evaluate_with_incompatible_unit(self, gaussian: Gaussian):
+        # WHEN
         x = sc.array(dims=["x"], values=[0.0, 500.0, 1000.0], unit="nm")
+
+        # THEN EXPECT
         with pytest.raises(
             UnitError,
             match="Input x has unit nm, but Gaussian component has unit meV. Failed to convert Gaussian to nm.",
@@ -84,14 +138,19 @@ class TestGaussian:
             gaussian.evaluate(x)
 
     def test_center_is_fixed_if_set_to_None(self):
+        # WHEN
         test_gaussian = Gaussian(
             name="TestGaussian", area=2.0, center=None, width=0.6, unit="meV"
         )
+        # THEN EXPECT
         assert test_gaussian._center.value == 0.0
         assert test_gaussian._center.fixed is True
 
     def test_get_parameters(self, gaussian: Gaussian):
+        # WHEN THEN
         params = gaussian.get_parameters()
+
+        # EXPECT
         assert len(params) == 3
         assert params[0].name == "TestGaussian area"
         assert params[1].name == "TestGaussian center"
@@ -105,22 +164,28 @@ class TestGaussian:
             gaussian._center.value + 10 * gaussian._width.value,
             1000,
         )
-        y = gaussian.evaluate(x)
-        numerical_area = simpson(y, x)
 
-        # THEN EXPECT
+        # THEN
+        y = gaussian.evaluate(x)
+
+        # EXPECT
+        numerical_area = simpson(y, x)
         assert np.isclose(numerical_area, gaussian._area.value, rtol=1e-3)
 
     def test_convert_unit(self, gaussian: Gaussian):
+        # WHEN THEN
         gaussian.convert_unit("microeV")
 
+        # EXPECT
         assert gaussian.unit == "microeV"
         assert gaussian._area.value == 2 * 1e3
         assert gaussian._center.value == 0.5 * 1e3
         assert gaussian._width.value == 0.6 * 1e3
 
     def test_copy(self, gaussian: Gaussian):
+        # WHEN THEN
         gaussian_copy = gaussian.copy()
+        # EXPECT
         assert gaussian_copy is not gaussian
         assert gaussian_copy.name == "copy of " + gaussian.name
 
@@ -136,7 +201,9 @@ class TestGaussian:
         assert gaussian_copy.unit == gaussian.unit
 
     def test_repr(self, gaussian: Gaussian):
+        # WHEN THEN
         repr_str = repr(gaussian)
+        # EXPECT
         assert "Gaussian" in repr_str
         assert "name = TestGaussian" in repr_str
         assert "unit = meV" in repr_str
