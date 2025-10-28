fac0bfad7e4db54b227b8c993d75286dc0233084
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2025-10-28 22:43:24 +0100

    Added some tests

--- tests/unit_tests/experiment/test_experiment.py
+++ tests/unit_tests/experiment/test_experiment.py
@@ -18,9 +18,34 @@ class TestExperiment:
         experiment = Experiment(name="test_experiment", data=data)
         return experiment
 
-    def test_init(self, experiment):
+    def test_init_array(self, experiment):
         # THEN EXPECT
         assert experiment.name == "test_experiment"
+        assert isinstance(experiment._data, sc.DataArray)
+        assert "Q" in experiment._data.dims
+        assert "energy" in experiment._data.dims
+        assert experiment._data.sizes["Q"] == 10
+        assert experiment._data.sizes["energy"] == 11
+        assert sc.identical(
+            experiment._data.data,
+            sc.array(dims=["Q", "energy"], values=np.ones((10, 11))),
+        )
+
+    def test_init_string(self, tmp_path):
+        # WHEN
+        Q = sc.linspace("Q", 0.5, 1.5, num=10, unit="1/Angstrom")
+        energy = sc.linspace("energy", -5, 5, num=11, unit="meV")
+        values = sc.array(dims=["Q", "energy"], values=np.ones((10, 11)))
+        data = sc.DataArray(data=values, coords={"Q": Q, "energy": energy})
+
+        filename = tmp_path / "test_experiment.h5"
+        sc.io.save_hdf5(data, filename)
+
+        # THEN
+        experiment = Experiment(name="loaded_experiment", data=str(filename))
+
+        # EXPECT
+        assert experiment.name == "loaded_experiment"
         assert isinstance(experiment._data, sc.DataArray)
         assert "Q" in experiment._data.dims
         assert "energy" in experiment._data.dims
@@ -60,6 +85,21 @@ class TestExperiment:
         assert sc.identical(data_to_save, loaded_data)
         assert new_experiment.name == "loaded_data"
 
+    def test_load_hdf5_invalid_name_raises(self, experiment):
+        # WHEN / THEN EXPECT
+        with pytest.raises(TypeError):
+            experiment.load_hdf5("some_file.h5", name=123)
+
+    def test_load_hdf5_invalid_filename_raises(self, experiment):
+        # WHEN / THEN EXPECT
+        with pytest.raises(TypeError):
+            experiment.load_hdf5(123)
+
+    def test_load_hdf5_invalid_file_raises(self, experiment):
+        # WHEN / THEN EXPECT
+        with pytest.raises(OSError):
+            experiment.load_hdf5("non_existent_file.h5")
+
     def test_save_hdf5(self, tmp_path, experiment):
         # WHEN THEN
         filename = tmp_path / "saved_data.h5"
@@ -70,27 +110,37 @@ class TestExperiment:
         original_data = experiment.data
         assert sc.identical(original_data, loaded_data)
 
+    def test_save_hdf5_default_name(self, tmp_path, experiment):
+        # WHEN THEN
+        current_dir = tmp_path
+        experiment.name = "default_name_experiment"
+        expected_filename = current_dir / "default_name_experiment.h5"
+        experiment.save_hdf5()
+
+        # EXPECT
+        loaded_data = sc.io.load_hdf5(str(expected_filename))
+        original_data = experiment.data
+        assert sc.identical(original_data, loaded_data)
+
+    def test_save_hdf5_no_data_raises(self):
+        # WHEN
+        experiment = Experiment(name="no_data_experiment")
+
+        # THEN EXPECT
+        with pytest.raises(AttributeError):
+            experiment.save_hdf5("should_fail.h5")
+
+    def test_save_hdf5_invalid_filename_raises(self, experiment):
+        # WHEN / THEN EXPECT
+        with pytest.raises(TypeError):
+            experiment.save_hdf5(123)
+
     def test_remove_data(self, experiment):
         # WHEN
         experiment.remove_data()
 
         # THEN EXPECT
         assert experiment._data is None
-
-    def test_load_hdf5_invalid_filename(self, experiment):
-        # WHEN / THEN EXPECT
-        with pytest.raises(TypeError):
-            experiment.load_hdf5(123)
-
-    def test_load_hdf5_invalid_file(self, experiment):
-        # WHEN / THEN EXPECT
-        with pytest.raises(OSError):
-            experiment.load_hdf5("non_existent_file.h5")
-
-    def test_save_hdf5_invalid_filename(self, experiment):
-        # WHEN / THEN EXPECT
-        with pytest.raises(TypeError):
-            experiment.save_hdf5(123)
 
     def test_repr(self, experiment):
         # WHEN
