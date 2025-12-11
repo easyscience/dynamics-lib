da2462cbcfdbc1af9868879ae037d1cc7b7bc213
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2025-12-11 13:32:29 +0100

    Update voigt

--- src/easydynamics/sample_model/components/voigt.py
+++ src/easydynamics/sample_model/components/voigt.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Optional, Union
+from typing import Union
 
 import numpy as np
 import scipp as sc
@@ -11,7 +11,7 @@ from easydynamics.sample_model.components.mixins import CreateParametersMixin
 
 from .model_component import ModelComponent
 
-Numeric = Union[float, int]
+Numeric = float | int
 
 
 class Voigt(CreateParametersMixin, ModelComponent):
@@ -20,7 +20,7 @@ class Voigt(CreateParametersMixin, ModelComponent):
     If the center is not provided, it will be centered at 0 and fixed, which is typically what you want in QENS.
 
     Args:
-        name (str): Name of the component.
+        display_name (str): Name of the component.
         center (Int or float or None): Center of the Voigt profile.
         gaussian_width (Int or float): Standard deviation of the Gaussian part.
         lorentzian_width (Int or float): Half width at half max (HWHM) of the Lorentzian part.
@@ -30,43 +30,94 @@ class Voigt(CreateParametersMixin, ModelComponent):
 
     def __init__(
         self,
-        name: Optional[str] = "Voigt",
-        area: Optional[Union[Numeric, Parameter]] = 1.0,
-        center: Optional[Union[Numeric, Parameter, None]] = None,
-        gaussian_width: Optional[Union[Numeric, Parameter]] = 1.0,
-        lorentzian_width: Optional[Union[Numeric, Parameter]] = 1.0,
-        unit: Optional[Union[str, sc.Unit]] = "meV",
+        display_name: str = "Voigt",
+        area: Numeric | Parameter = 1.0,
+        center: Numeric | Parameter | None = None,
+        gaussian_width: Numeric | Parameter = 1.0,
+        lorentzian_width: Numeric | Parameter = 1.0,
+        unit: str | sc.Unit = "meV",
     ):
         # Validate inputs and create Parameters if not given
         self.validate_unit(unit)
         self._unit = unit
 
         # These methods live in ValidationMixin
-        area = self._create_area_parameter(area=area, name=name, unit=self._unit)
+        area = self._create_area_parameter(
+            area=area, name=display_name, unit=self._unit
+        )
         center = self._create_center_parameter(
-            center=center, name=name, fix_if_none=True, unit=self._unit
+            center=center, name=display_name, fix_if_none=True, unit=self._unit
         )
         gaussian_width = self._create_width_parameter(
             width=gaussian_width,
-            name=name,
+            name=display_name,
             param_name="gaussian_width",
             unit=self._unit,
         )
         lorentzian_width = self._create_width_parameter(
             width=lorentzian_width,
-            name=name,
+            name=display_name,
             param_name="lorentzian_width",
             unit=self._unit,
         )
 
         super().__init__(
-            name=name,
+            display_name=display_name,
             unit=unit,
-            area=area,
-            center=center,
-            gaussian_width=gaussian_width,
-            lorentzian_width=lorentzian_width,
         )
+
+        self._area = area
+        self._center = center
+        self._gaussian_width = gaussian_width
+        self._lorentzian_width = lorentzian_width
+
+    @property
+    def area(self) -> Parameter:
+        """Get the area parameter."""
+        return self._area
+
+    @area.setter
+    def area(self, value: Numeric) -> None:
+        """Set the area parameter value."""
+        if not isinstance(value, Numeric):
+            raise TypeError("area must be a number")
+        self._area.value = value
+
+    @property
+    def center(self) -> Parameter:
+        """Get the center parameter."""
+        return self._center
+
+    @center.setter
+    def center(self, value: Numeric) -> None:
+        """Set the center parameter value."""
+        if not isinstance(value, Numeric):
+            raise TypeError("center must be a number")
+        self._center.value = value
+
+    @property
+    def gaussian_width(self) -> Parameter:
+        """Get the width parameter."""
+        return self._gaussian_width
+
+    @gaussian_width.setter
+    def gaussian_width(self, value: Numeric) -> None:
+        """Set the width parameter value."""
+        if not isinstance(value, Numeric):
+            raise TypeError("gaussian_width must be a number")
+        self._gaussian_width.value = value
+
+    @property
+    def lorentzian_width(self) -> Parameter:
+        """Get the width parameter."""
+        return self._lorentzian_width
+
+    @lorentzian_width.setter
+    def lorentzian_width(self, value: Numeric) -> None:
+        """Set the width parameter value."""
+        if not isinstance(value, Numeric):
+            raise TypeError("lorentzian_width must be a number")
+        self._lorentzian_width.value = value
 
     def evaluate(
         self, x: Union[Numeric, list, np.ndarray, sc.Variable, sc.DataArray]
@@ -84,4 +135,4 @@ class Voigt(CreateParametersMixin, ModelComponent):
         )
 
     def __repr__(self):
-        return f"Voigt(name = {self.name}, unit = {self._unit},\n area = {self.area},\n center = {self.center},\n gaussian_width = {self.gaussian_width},\n lorentzian_width = {self.lorentzian_width})"
+        return f"Voigt(display_name = {self.display_name}, unit = {self._unit},\n area = {self.area},\n center = {self.center},\n gaussian_width = {self.gaussian_width},\n lorentzian_width = {self.lorentzian_width})"
