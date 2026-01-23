e559071cc930ef0abc171b05f60ba3d8f754dfdd
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2026-01-23 16:25:10 +0100

    update global object

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -2,15 +2,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import pytest
-from easyscience.global_object.global_object import GlobalObject
+
+# from easyscience.global_object.global_object import GlobalObject
+from easyscience import global_object
 
 # TODO: remove once weakref bug is fixed
 
 
 @pytest.fixture(autouse=True)
 def reset_global_object():
-    # Clear the existing global object
-    global_obj = GlobalObject()
-    global_obj.map._store.clear()
-    global_obj.map._Map__type_dict.clear()  # private dict, needed for weakref finalizers
-    yield
+    global_object.map._clear()
