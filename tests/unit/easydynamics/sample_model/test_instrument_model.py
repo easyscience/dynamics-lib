5ee4852f0ce7be164c198594ed7598a5ccee23eb
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-03-05 02:01:31 +0100

    Update SPDX headers

--- tests/unit/easydynamics/sample_model/test_instrument_model.py
+++ tests/unit/easydynamics/sample_model/test_instrument_model.py
@@ -1,6 +1,5 @@
-# SPDX-FileCopyrightText: 2025-2026 EasyDynamics contributors <https://github.com/easyscience>
+# SPDX-FileCopyrightText: 2026 EasyScience contributors <https://github.com/easyscience>
 # SPDX-License-Identifier: BSD-3-Clause
-
 
 from unittest.mock import MagicMock
 from unittest.mock import patch
