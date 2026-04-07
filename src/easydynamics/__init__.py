4b3cce00596c40db0ab9c66dfb8ad40368a621c9
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2026-04-07 15:02:48 +0200

    Update tutorial0

--- src/easydynamics/__init__.py
+++ src/easydynamics/__init__.py
@@ -1,3 +1,11 @@
 # SPDX-FileCopyrightText: 2025 EasyScience contributors <https://github.com/easyscience>
 # SPDX-License-Identifier: BSD-3-Clause
 """EasyDynamics library."""
+
+from .analysis import Analysis
+from .experiment import Experiment
+
+__all__ = [
+    "Analysis",
+    "Experiment",
+]
