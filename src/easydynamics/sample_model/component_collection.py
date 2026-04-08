0a57f3331e243662937e0b76d59dff6091c7a145
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2026-04-08 20:04:02 +0200

    Add A, COM, FA linting and upgrade python version

--- src/easydynamics/sample_model/component_collection.py
+++ src/easydynamics/sample_model/component_collection.py
@@ -35,9 +35,9 @@ class ComponentCollection(ModelBase):
 
         Parameters
         ----------
-        unit : str | sc.Unit, default="meV"
+        unit : str | sc.Unit, default='meV'
             Unit of the collection.
-        display_name : str | None, default="MyComponentCollection"
+        display_name : str | None, default='MyComponentCollection'
             Display name of the collection.
         unique_name : str | None, default=None
             Unique name of the collection.
