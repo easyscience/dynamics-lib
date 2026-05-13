6b995a8d3e58764fe4a251355a75223c521f8835
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2026-05-13 09:47:49 +0200

    Add DeltaLorentz model

---
+++ src/easydynamics/sample_model/diffusion_model/delta_lorentz.py
@@ -0,0 +1,577 @@
+# SPDX-FileCopyrightText: 2026 EasyScience contributors <https://github.com/easyscience>
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+import numpy as np
+import scipp as sc
+from easyscience.variable import DescriptorNumber
+from easyscience.variable import Parameter
+
+from easydynamics.sample_model.component_collection import ComponentCollection
+from easydynamics.sample_model.components import DeltaFunction
+from easydynamics.sample_model.components import Lorentzian
+from easydynamics.sample_model.diffusion_model.diffusion_model_base import (
+    DiffusionModelBase,
+)
+from easydynamics.utils.utils import Numeric
+from easydynamics.utils.utils import Q_type
+from easydynamics.utils.utils import _validate_and_convert_Q
+from easydynamics.utils.utils import angstrom
+from easydynamics.utils.utils import hbar
+
+
+class DeltaLorentz(DiffusionModelBase):
+    r"""
+    Model of Delta function and Lorentzian with intensities given by the Debye-Waller factor.
+    $$
+    I = K \exp \left( \frac{-\langle u^2 \rangle Q^2}{3} \right)[A_0 \delta(E) + (A_1) L(E, \Gamma)]
+    $$,
+
+    where $K$ is the scale factor, $\langle u^2 \rangle$ is the mean square displacement, $Q$ is the scattering vector, $A_0$ and $A_1$ are the amplitudes of the delta function and Lorentzian, respectively, and $L(E, \Gamma)$ is the Lorentzian function with width $\Gamma$.
+    $A_0+A_1=1$ and $A_0$ is the EISF, while $A_1$ is the QISF.
+
+    Creates ComponentCollections with Lorentzian components for given
+    Q-values.
+
+    Example
+    --------
+    >>> Q=np.linspace(0.5,2,7) >>>energy=np.linspace(-2, 2, 501) >>>scale=1.0
+    >>> diffusion_coefficient = 2.4e-9  # m^2/s
+    >>> diffusion_model=DeltaLorentz(display_name="DiffusionModel",
+    >>> scale=scale, diffusion_coefficient= diffusion_coefficient,)
+    >>> component_collections=diffusion_model.create_component_collections(Q)
+
+    See also the
+    tutorials.
+    """
+
+    def __init__(
+        self,
+        display_name: str | None = "DeltaLorentz",
+        unique_name: str | None = None,
+        unit: str | sc.Unit = "meV",
+        scale: Numeric = 1.0,
+        mean_u_squared: Numeric = 0.0,
+        A_0: Numeric = 1.0,
+        lorentzian_width: Numeric = 1.0,
+    ) -> None:
+        """
+        Initialize a new DeltaLorentz model.
+
+        Parameters
+        ----------
+        display_name : str | None, default='DeltaLorentz'
+            Display name of the diffusion model.
+        unique_name : str | None, default=None
+            Unique name of the diffusion model. If None, a unique name will be generated. By
+            default, None.
+        unit : str | sc.Unit, default='meV'
+            Unit of the diffusion model. Must be convertible to meV.
+        scale : Numeric, default=1.0
+            Scale factor for the diffusion model. Must be a non-negative number.
+        mean_u_squared : Numeric, default=0.0
+            Mean square displacement in angstrom^2.
+        A_0 : Numeric, default=1.0
+            Amplitude of the delta function.
+        lorentzian_width : Numeric, default=1.0
+            Width of the Lorentzian function.
+
+        Raises
+        ------
+        TypeError
+            If scale or diffusion_coefficient is not a number.
+        """
+        if not isinstance(scale, Numeric):
+            raise TypeError("scale must be a number.")
+
+        if not isinstance(mean_u_squared, Numeric):
+            raise TypeError("mean_u_squared must be a number.")
+
+        if not isinstance(A_0, Numeric):
+            raise TypeError("A_0 must be a number.")
+
+        if not isinstance(lorentzian_width, Numeric):
+            raise TypeError("lorentzian_width must be a number.")
+
+        super().__init__(
+            display_name=display_name,
+            unique_name=unique_name,
+            unit=unit,
+            scale=scale,
+        )
+        self._hbar = hbar
+        self._angstrom = angstrom
+        self._mean_u_squared = mean_u_squared
+
+        A_0 = Parameter(
+            name="A_0",
+            value=float(A_0),
+            fixed=False,
+            min=0.0,
+            max=1.0,
+        )
+        A_1 = Parameter.from_dependency(
+            name="A_1",
+            dependency_expression="1 - A_0",
+            dependency_map={"A_0": A_0},
+        )
+
+        mean_u_squared = Parameter(
+            name="mean_u_squared",
+            value=float(mean_u_squared),
+            fixed=False,
+            min=0.0,
+            unit="angstrom**2",
+        )
+        self._mean_u_squared = mean_u_squared
+
+        lorentzian_width = Parameter(
+            name="lorentzian_width",
+            value=float(lorentzian_width),
+            fixed=False,
+            min=0.0,
+            unit=unit,
+        )
+
+        self._A_0 = A_0
+        self._A_1 = A_1
+        self._lorentzian_width = lorentzian_width
+
+    # ------------------------------------------------------------------
+    # Properties
+    # ------------------------------------------------------------------
+
+    @property
+    def mean_u_squared(self) -> Parameter:
+        """
+        Get the mean square displacement parameter.
+
+        Returns
+        -------
+        Parameter
+            Mean square displacement in angstrom^2.
+        """
+        return self._mean_u_squared
+
+    @mean_u_squared.setter
+    def mean_u_squared(self, mean_u_squared: Numeric) -> None:
+        """
+        Set the mean square displacement parameter.
+
+        Parameters
+        ----------
+        mean_u_squared : Numeric
+            The new value for the mean square displacement in angstrom^2.
+
+        Raises
+        ------
+        TypeError
+            If mean_u_squared is not a number.
+        ValueError
+            If mean_u_squared is negative.
+        """
+        if not isinstance(mean_u_squared, Numeric):
+            raise TypeError("mean_u_squared must be a number.")
+
+        if float(mean_u_squared) < 0:
+            raise ValueError("mean_u_squared must be non-negative.")
+        self._mean_u_squared.value = float(mean_u_squared)
+
+    @property
+    def A_0(self) -> Parameter:
+        """
+        Get the amplitude of the delta function.
+
+        Returns
+        -------
+        Parameter
+            Amplitude of the delta function.
+        """
+        return self._A_0
+
+    @A_0.setter
+    def A_0(self, A_0: Numeric) -> None:
+        """
+        Set the amplitude of the delta function.
+
+        Parameters
+        ----------
+        A_0 : Numeric
+            The new value for the amplitude of the delta function. Must be between 0 and 1.
+
+        Raises
+        ------
+        TypeError
+            If A_0 is not a number.
+        ValueError
+            If A_0 is not between 0 and 1.
+        """
+        if not isinstance(A_0, Numeric):
+            raise TypeError("A_0 must be a number.")
+
+        if not (0 <= float(A_0) <= 1):
+            raise ValueError("A_0 must be between 0 and 1.")
+        self._A_0.value = float(A_0)
+
+    @property
+    def A_1(self) -> Parameter:
+        """
+        Get the amplitude of the Lorentzian function.
+
+        Returns
+        -------
+        Parameter
+            Amplitude of the Lorentzian function.
+        """
+        return self._A_1
+
+    @A_1.setter
+    def A_1(self, _A_1: Numeric) -> None:
+        """
+        A_1 cannot be set directly, as it is a dependent parameter defined as 1 - A_0. To change A_1, set A_0 to the desired value and A_1 will update accordingly.
+
+
+        Parameters
+        ----------
+        _A_1 : Numeric
+            The new value for the amplitude of the Lorentzian function. Is ignored
+
+        Raises
+        ------
+         AttributeError
+             If an attempt is made to set A_1 directly.
+        """
+        raise AttributeError(
+            "A_1 is a dependent parameter and cannot be set directly. Set A_0 to change A_1 accordingly."
+        )
+
+    @property
+    def lorentzian_width(self) -> Parameter:
+        """
+        Get the width of the Lorentzian function.
+
+        Returns
+        -------
+        Parameter
+            Width of the Lorentzian function.
+        """
+        return self._lorentzian_width
+
+    @lorentzian_width.setter
+    def lorentzian_width(self, lorentzian_width: Numeric) -> None:
+        """
+        Set the width of the Lorentzian function.
+
+        Parameters
+        ----------
+        lorentzian_width : Numeric
+            The new value for the width of the Lorentzian function. Must be a non-negative number.
+
+        Raises
+        ------
+        TypeError
+            If lorentzian_width is not a number.
+        ValueError
+            If lorentzian_width is negative.
+        """
+        if not isinstance(lorentzian_width, Numeric):
+            raise TypeError("lorentzian_width must be a number.")
+
+        if float(lorentzian_width) < 0:
+            raise ValueError("lorentzian_width must be non-negative.")
+        self._lorentzian_width.value = float(lorentzian_width)
+
+    # ------------------------------------------------------------------
+    # Other methods
+    # ------------------------------------------------------------------
+
+    def calculate_width(self, Q: Q_type) -> np.ndarray:
+        """
+        Calculate the half-width at half-maximum (HWHM) for the diffusion model.
+
+        Parameters
+        ----------
+        Q : Q_type
+            Scattering vector in 1/angstrom.
+
+        Returns
+        -------
+        np.ndarray
+            HWHM values in the unit of the model (e.g., meV).
+        """
+
+        Q = _validate_and_convert_Q(Q)
+
+        return self.lorentzian_width.value * np.ones_like(Q)
+
+    def calculate_EISF(self, Q: Q_type) -> np.ndarray:
+        """
+        Calculate the Elastic Incoherent Structure Factor (EISF) for the Brownian translational
+        diffusion model.
+
+        Parameters
+        ----------
+        Q : Q_type
+            Scattering vector in 1/angstrom.
+
+        Returns
+        -------
+        np.ndarray
+            EISF values (dimensionless).
+        """
+
+        # Need to handle units better
+        Q = _validate_and_convert_Q(Q)
+        return (
+            self.scale.value
+            * np.exp(-self.mean_u_squared.value * Q**2 / 3)
+            * self.A_0.value
+        )
+
+    def calculate_QISF(self, Q: Q_type) -> np.ndarray:
+        """
+        Calculate the Quasi-Elastic Incoherent Structure Factor (QISF).
+
+        Parameters
+        ----------
+        Q : Q_type
+            Scattering vector in 1/angstrom.
+
+        Returns
+        -------
+        np.ndarray
+            QISF values (dimensionless).
+        """
+
+        Q = _validate_and_convert_Q(Q)
+        return (
+            self.scale.value
+            * np.exp(-self.mean_u_squared.value * Q**2 / 3)
+            * self.A_1.value
+        )
+
+    def create_component_collections(
+        self,
+        Q: Q_type,
+        component_display_name: str = "DeltaLorentz component",
+    ) -> list[ComponentCollection]:
+        r"""
+        Create ComponentCollection components for the DeltaLorentz model at
+        given Q values.
+
+        Parameters
+        ----------
+        Q : Q_type
+            Scattering vector values.
+        component_display_name : str, default='DeltaLorentz component'
+            Name of the Lorentzian component.
+
+        Raises
+        ------
+        TypeError
+            If component_display_name is not a string.
+
+        Returns
+        -------
+        list[ComponentCollection]
+            List of ComponentCollections with Lorentzian and delta functioncomponents for each Q value.
+        """
+        Q = _validate_and_convert_Q(Q)
+
+        if not isinstance(component_display_name, str):
+            raise TypeError("component_name must be a string.")
+
+        component_collection_list = [None] * len(Q)
+        # In more complex models, this is used to scale the area of the
+        # Lorentzians and the delta function.
+        QISF = self.calculate_QISF(Q)
+
+        # Create a Lorentzian component for each Q-value, with
+        # width D*Q^2 and area equal to scale.
+        # No delta function, as the EISF is 0.
+        for i, Q_value in enumerate(Q):
+            component_collection_list[i] = ComponentCollection(
+                display_name=f"{self.display_name}_Q{Q_value:.2f}", unit=self.unit
+            )
+
+            lorentzian_component = Lorentzian(
+                display_name=component_display_name,
+                unit=self.unit,
+            )
+
+            # Make the width dependent on Q
+
+            lorentzian_component.width.make_dependent_on(
+                dependency_expression=self._write_width_dependency_expression(Q_value),
+                dependency_map=self._write_width_dependency_map_expression(),
+                desired_unit=self.unit,
+            )
+
+            # Make the area dependent on Q
+            lorentzian_component.area.make_dependent_on(
+                dependency_expression=self._write_lorz_area_dependency_expression(
+                    Q_value
+                ),
+                dependency_map=self._write_lorz_area_dependency_map_expression(),
+            )
+
+            component_collection_list[i].append_component(lorentzian_component)
+
+            delta_component = DeltaFunction(
+                display_name="Delta function", unit=self.unit
+            )
+            delta_component.area.make_dependent_on(
+                dependency_expression=self._write_delta_area_dependency_expression(
+                    Q_value
+                ),
+                dependency_map=self._write_delta_area_dependency_map_expression(),
+            )
+
+            component_collection_list[i].append_component(delta_component)
+
+        return component_collection_list
+
+    # ------------------------------------------------------------------
+    # Private methods
+    # ------------------------------------------------------------------
+
+    def _write_width_dependency_expression(self, Q: float) -> str:
+        """
+        Write the dependency expression for the width as a function of Q to make dependent
+        Parameters.
+
+        Parameters
+        ----------
+        Q : float
+            Scattering vector in 1/angstrom.
+
+        Raises
+        ------
+        TypeError
+            If Q is not a float.
+
+        Returns
+        -------
+        str
+            Dependency expression for the width.
+        """
+        if not isinstance(Q, (float)):
+            raise TypeError("Q must be a float.")
+
+        # Q is given as a float, so we need to add the units
+        return "lorentzian_width"
+
+    def _write_width_dependency_map_expression(self) -> dict[str, DescriptorNumber]:
+        """
+        Write the dependency map expression to make dependent Parameters.
+
+        Returns
+        -------
+        dict[str, DescriptorNumber]
+            Dependency map for the width.
+        """
+        return {
+            "lorentzian_width": self.lorentzian_width,
+        }
+
+    def _write_lorz_area_dependency_expression(self, Q) -> str:
+        """
+        Write the dependency expression for the area to make dependent Parameters.
+
+        Parameters
+        ----------
+        QISF : float
+            Quasielastic Incoherent Scattering Function.
+
+        Raises
+        ------
+        TypeError
+            If QISF is not a float.
+
+        Returns
+        -------
+        str
+            Dependency expression for the area.
+        """
+        if not isinstance(Q, (float)):
+            raise TypeError("Q must be a float.")
+
+        return f"scale * exp(-mean_u_squared.value * {Q}**2 / 3) * A_1"
+
+    def _write_lorz_area_dependency_map_expression(self) -> dict[str, DescriptorNumber]:
+        """
+        Write the dependency map expression to make dependent Parameters.
+
+        Returns
+        -------
+        dict[str, DescriptorNumber]
+            Dependency map for the area.
+        """
+        return {
+            "scale": self.scale,
+            "mean_u_squared": self.mean_u_squared,
+            "A_1": self.A_1,
+        }
+
+    def _write_delta_area_dependency_expression(self, Q) -> str:
+        """
+        Write the dependency expression for the area to make dependent Parameters.
+
+        Parameters
+        ----------
+        QISF : float
+            Quasielastic Incoherent Scattering Function.
+
+        Raises
+        ------
+        TypeError
+            If QISF is not a float.
+
+        Returns
+        -------
+        str
+            Dependency expression for the area.
+        """
+        if not isinstance(Q, (float)):
+            raise TypeError("Q must be a float.")
+
+        return f"scale * exp(-mean_u_squared.value * {Q}**2 / 3) * A_0"
+
+    def _write_delta_area_dependency_map_expression(
+        self,
+    ) -> dict[str, DescriptorNumber]:
+        """
+        Write the dependency map expression to make dependent Parameters.
+
+        Returns
+        -------
+        dict[str, DescriptorNumber]
+            Dependency map for the area.
+        """
+        return {
+            "scale": self.scale,
+            "mean_u_squared": self.mean_u_squared,
+            "A_0": self.A_0,
+        }
+
+    # ------------------------------------------------------------------
+    # dunder methods
+    # ------------------------------------------------------------------
+
+    def __repr__(self) -> str:
+        """
+        String representation of the DeltaLorentz model.
+
+        Returns
+        -------
+        str
+            String representation of the DeltaLorentz model.
+        """
+        return (
+            f"DeltaLorentz(display_name={self.display_name},"
+            f"unit={self.unit}, \n"
+            f"    mean_u_squared={self.mean_u_squared}, \n"
+            f"    A_0={self.A_0}, A_1={self.A_1}, \n"
+            f"    lorentzian_width={self.lorentzian_width}, \n"
+            f"    scale={self.scale})"
+        )
