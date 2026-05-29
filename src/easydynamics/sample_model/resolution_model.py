0861fec88a51e9f52ad5d38c40e3c3c88709f18b
henrikjacobsenfys <henrik.jacobsen.fys@gmail.com>
2026-05-29 20:38:50 +0200

    implement from_sample_model

--- src/easydynamics/sample_model/resolution_model.py
+++ src/easydynamics/sample_model/resolution_model.py
@@ -6,8 +6,10 @@ import scipp as sc
 from easydynamics.sample_model.component_collection import ComponentCollection
 from easydynamics.sample_model.components import DeltaFunction
 from easydynamics.sample_model.components import Polynomial
+from easydynamics.sample_model.components.exponential import Exponential
 from easydynamics.sample_model.components.model_component import ModelComponent
 from easydynamics.sample_model.model_base import ModelBase
+from easydynamics.sample_model.sample_model import SampleModel
 from easydynamics.utils.utils import Q_type
 
 
@@ -29,11 +31,11 @@ class ResolutionModel(ModelBase):
 
         Parameters
         ----------
-        display_name : str, default='MyResolutionModel'
+        display_name : str, default="MyResolutionModel"
             Display name of the model.
         unique_name : str | None, default=None
             Unique name of the model. If None, a unique name will be generated.
-        unit : str | sc.Unit, default='meV'
+        unit : str | sc.Unit, default="meV"
             Unit of the model.
         components : ModelComponent | ComponentCollection | None, default=None
             Template components of the model. If None, no components are added. These components
@@ -70,9 +72,71 @@ class ResolutionModel(ModelBase):
         components = component if isinstance(component, ComponentCollection) else (component,)
 
         for comp in components:
-            if isinstance(comp, (DeltaFunction, Polynomial)):
+            if isinstance(comp, (DeltaFunction, Polynomial, Exponential)):
                 raise TypeError(
                     f'Component in ResolutionModel cannot be a {comp.__class__.__name__}'
                 )
 
         super().append_component(component)
+
+    @classmethod
+    def from_sample_model(
+        cls,
+        sample_model: SampleModel,
+        normalize_area: bool = True,
+        fix_parameters: bool = True,
+    ) -> 'ResolutionModel':
+        """
+        Create a ResolutionModel from a SampleModel.
+
+        Parameters
+        ----------
+        sample_model : SampleModel
+            SampleModel to create the ResolutionModel from.
+        normalize_area : bool, default=True
+            Whether to normalize the components in the ResolutionModel to have area 1.
+        fix_parameters : bool, default=True
+            Whether to fix the parameters in the ResolutionModel.
+
+        Returns
+        -------
+        "ResolutionModel"
+            ResolutionModel created from the SampleModel.
+
+        Raises
+        ------
+        TypeError
+            If sample_model is not a SampleModel, or if normalize_area or fix_parameters are not
+            bool.
+        """
+        if not isinstance(sample_model, SampleModel):
+            raise TypeError(
+                f'sample_model must be an instance of SampleModel. Got {type(sample_model)}.'
+            )
+
+        if not isinstance(normalize_area, bool):
+            raise TypeError('normalize_area must be True or False.')
+
+        if not isinstance(fix_parameters, bool):
+            raise TypeError('fix_parameters must be True or False.')
+
+        resolution_model = cls(
+            display_name=sample_model.display_name,
+            unit=sample_model.unit,
+            components=sample_model.components,
+            Q=sample_model.Q,
+        )
+
+        from copy import copy
+
+        for index in range(len(sample_model.Q)):
+            resolution_model._component_collections[index] = copy(
+                sample_model.get_component_collection(Q_index=index)
+            )
+        if normalize_area:
+            resolution_model.normalize_area()
+
+        if fix_parameters:
+            resolution_model.fix_all_parameters()
+
+        return resolution_model
